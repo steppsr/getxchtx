a28c6fa73cb51977af6fb62086ba3660700f678c
Steve Stepp <steppsr@gmail.com>
2022-04-08 17:12:21 -0500

    Initial commit

---
+++ taxtran.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+path=~/sourcecode/taxtran
+
+curl -s -X POST --insecure --cert ~/.chia/mainnet/config/ssl/wallet/private_wallet.crt --key ~/.chia/mainnet/config/ssl/wallet/private_wallet.key -H "Accept: application/json" -H "Content-Type: application/json" "https://localhost:9256/get_transactions" -d '{"wallet_id":1,"start":0,"end":99999}' | python3 -m json.tool | grep -E 'name' | cut --fields 4 --delimiter=\" | cut -c 3- > trans_id_list.txt
+
+cnt=`cat $path/trans_id_list.txt | wc -l`
+
+while read t; do
+  $path/chiatx.sh $t $cnt
+  cnt=$((cnt-1))
+done <$path/trans_id_list.txt
