d7b1b50a42650f68fbcedb1b765deaf76248d097
Steve Stepp <steppsr@gmail.com>
2022-04-09 00:35:28 -0500

    Added command line options

--- taxtran.sh
+++ taxtran.sh
@@ -1,27 +1,48 @@
-<<<<<<< HEAD
 #!/bin/bash
 
-path=~/sourcecode/taxtran
+usage () {
+  echo "USAGE: sh taxtran.sh [OPTIONS]"
+  echo ""
+  echo "OPTIONS"
+  echo "  -y        4-digit year for transactions"
+  echo "  -d        directory to store output file"
+  echo "  -f        output filename"
+  echo ""
+  echo "Example:  taxtran -y 2021 -d ~/mytaxes -f transactions.csv"
+  echo ""
+}
 
-curl -s -X POST --insecure --cert ~/.chia/mainnet/config/ssl/wallet/private_wallet.crt --key ~/.chia/mainnet/config/ssl/wallet/private_wallet.key -H "Accept: application/json" -H "Content-Type: application/json" "https://localhost:9256/get_transactions" -d '{"wallet_id":1,"start":0,"end":99999}' | python3 -m json.tool | grep -E 'name' | cut --fields 4 --delimiter=\" | cut -c 3- > trans_id_list.txt
+year="all"
+dir=`pwd`
+file="xch_transactions.csv"
 
-=======
-#/!bin/bash
+while [ -n "$1" ]
+do
 
-path=~/sourcecode/taxtran
+  case "$1" in
+    -h) usage
+         exit 1
+         ;;
+    -y) year=$2
+        shift ;;
+    -d) dir=$2
+        shift ;;
+    -f) file=$2
+        shift ;;
+    --) shift
+        break ;;
+    *)  ;;
+  esac
+shift
+done
 
-# get a list of transaction ids
-curl -s -X POST --insecure --cert ~/.chia/mainnet/config/ssl/wallet/private_wallet.crt --key ~/.chia/mainnet/config/ssl/wallet/private_wallet.key -H "Accept: application/json" -H "Content-Type: application/json" "https://localhost:9256/get_transactions" -d '{"wallet_id":1,"start":0,"end":99999}' | python3 -m json.tool | grep -E 'name' | cut --fields 4 --delimiter=\" | cut -c 3- >$path/trans_id_list.txt
->>>>>>> Initial commit
-cnt=`cat $path/trans_id_list.txt | wc -l`
+path="$dir/$file"
+
+curl -s -X POST --insecure --cert ~/.chia/mainnet/config/ssl/wallet/private_wallet.crt --key ~/.chia/mainnet/config/ssl/wallet/private_wallet.key -H "Accept: application/json" -H "Content-Type: application/json" "https://localhost:9256/get_transactions" -d '{"wallet_id":1,"start":0,"end":99999}' | python3 -m json.tool | grep -E 'name' | cut --fields 4 --delimiter=\" | cut -c 3- > $dir/trans_id_list.txt
+
+cnt=`cat $dir/trans_id_list.txt | wc -l`
 
 while read t; do
-  $path/chiatx.sh $t $cnt
+  $dir/chiatx.sh $t $path $year $cnt
   cnt=$((cnt-1))
-done <$path/trans_id_list.txt
-<<<<<<< HEAD
-=======
-
-cat $path/transactions.csv
-
->>>>>>> Initial commit
+done <$dir/trans_id_list.txt
